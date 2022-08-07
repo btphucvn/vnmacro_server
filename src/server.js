403ffb425d40641dced8273a1de5a6f2ea1806be
btphucvn <btphucvn@gmail.com>
2022-08-07 22:48:38 +0700

    update

--- src/server.js
+++ src/server.js
@@ -13,26 +13,26 @@ let app = express();
 //app.use(cors({ origin: true }));
 
 // Add headers before the routes are defined
-// app.use(function (req, res, next) {
+app.use(function (req, res, next) {
 
-//     // Website you wish to allow to connect
-//     //console.log(process.env.URL_REACT);
-//     //console.log(process);
+    // Website you wish to allow to connect
+    //console.log(process.env.URL_REACT);
+    //console.log(process);
 
-//     res.setHeader('Access-Control-Allow-Origin', process.env.URL_REACT);
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', process.env.URL_REACT);
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
-//     // Pass to next layer of middleware
-//     next();
-// });
+    // Pass to next layer of middleware
+    next();
+});
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
